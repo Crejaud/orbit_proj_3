4e7d0f70ddba04148df13e9ae372617020c289e1
crejaud <crejaud@gmail.com>
2016-12-13 20:23:18 -0500

    trying to get part3 to work

--- part3/bfs.cu
+++ part3/bfs.cu
@@ -6,23 +6,23 @@
 
 using namespace std;
 
-int iterative_bfs(int **matrix, unsigned int N, int target, bool **visited_matrix);
-int recursive_bfs(int **matrix, unsigned int N, int target, bool **visited_matrix);
-int recursive_bfs_helper(int **matrix, unsigned int N, int target, int x, int y, bool **visited_matrix);
+short iterative_bfs(short **matrix, unsigned long long N, short target, bool **visited_matrix);
+short recursive_bfs(short **matrix, unsigned long long N, short target, bool **visited_matrix);
+short recursive_bfs_helper(short **matrix, unsigned long long N, short target, unsigned long long x, unsigned long long y, bool **visited_matrix);
 
-void kernel_bfs_wrapper(int **matrix, int *result, int *mtx, unsigned int N, int target, bool **visited_matrix);
-__global__ void bfs_kernel(int **matrix, int *target, int *mtx, unsigned int N, int target, bool **visited_matrix);
+void kernel_bfs_wrapper(short **matrix, short *result, int *mtx, unsigned long long N, short target);
+__global__ void bfs_kernel(short **matrix, short *result, int *mtx, unsigned long long N, short target);
 
 int main()
 {
   // default size is 100
-  unsigned int N = 100;
-  int **seq_matrix,
+  unsigned long long N = 100;
+  short **seq_matrix,
   **cuda_matrix,
   *seq_result,
   *cuda_result,
-  *mtx,
   target;
+  int *mtx;
   bool **visited_matrix, **cuda_visited_matrix;
 
   // Declare timers
@@ -37,51 +37,54 @@ int main()
   cin >> target;
 
   // allocate memory for seq
-  seq_matrix = (int**) malloc(N * sizeof(int*));
+  seq_matrix = (short**) malloc(N * sizeof(short*));
   visited_matrix = (bool**) malloc(N * sizeof(bool*));
+  seq_result = (short*) malloc(sizeof(short));
 
-  // set matrix to random integers from 0 to 100
-  for (int i = 0; i < N; i++) {
-    *seq_matrix = (int*) malloc(N * sizeof(int));
-    *visited_matrix = (bool*) malloc(N * sizeof(bool));
-    for (int j = 0; j < N; j++) {
+  // set matrix to random shortegers from 0 to 100
+  for (unsigned long long i = 0; i < N; i++) {
+    seq_matrix[i] = (short*) malloc(N * sizeof(short));
+    visited_matrix[i] = (bool*) malloc(N * sizeof(bool));
+    for (unsigned long long j = 0; j < N; j++) {
       seq_matrix[i][j] = rand() % 101;
       visited_matrix[i][j] = false;
     }
   }
+  cout << "random numbers generated" << endl;
 
   // allocate memory for cuda
-  cudaMalloc((void**)&cuda_matrix, N * sizeof(int*));
-  for (int i = 0; i < N; i++) {
-    cudaMalloc((void**)&cuda_matrix[i], N * sizeof(int));
+  cudaMalloc((void**)&cuda_matrix, N * sizeof(short*));
+  for (unsigned long long i = 0; i < N; i++) {
+    cudaMalloc((void**)&cuda_matrix[i], N * sizeof(short));
   }
-  cudaMalloc((void**)&cuda_result, sizeof(int));
+  cudaMalloc((void**)&cuda_result, sizeof(short));
+  cudaMalloc((void**)&mtx, sizeof(int));
+  cout << "cuda malloc good" << endl;
 
   // set values of cuda target to -1 (not found)
   // set values of mtx target to 0
-  cudaMemset(cuda_result, -1, sizeof(int));
-  cudaMemset(mtx, 0, sizeof(int));
+  cudaMemset(cuda_result, -1, sizeof(short));
+  cudaMemset(mtx, 0, sizeof(short));
 
   // set up timing variables
   cudaEventCreate(&cuda_start);
   cudaEventCreate(&cuda_stop);
-
+  for (unsigned long long i = 0; i < N; i++) {
+    cudaMemcpy(cuda_matrix[i], seq_matrix[i], N * sizeof(short), cudaMemcpyHostToDevice);
+    //cudaMemcpy(cuda_visited_matrix[i], visited_matrix[i], N * sizeof(bool), cudaMemcpyHostToDevice);
+  }
   // copy from host to device
   cudaEventRecord(cuda_start, 0);
-  for (int i = 0; i < N; i++) {
-    cudaMemcpy(cuda_matrix[i], seq_matrix[i], N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(cuda_visited_matrix[i], visited_matrix[i], N * sizeof(bool), cudaMemcpyHostToDevice);
-  }
 
   // START CUDA
-  kernel_bfs_wrapper(cuda_matrix, cuda_result, mtx, N, target, cuda_visited_matrix);
+  kernel_bfs_wrapper(cuda_matrix, cuda_result, mtx, N, target);
 
   // copy from device to host
-  cudaMemcpy(seq_result, cuda_result, sizeof(int), cudaMemcpyDeviceToHost);
   cudaEventRecord(cuda_stop, 0);
   cudaEventSynchronize(cuda_stop);
   cudaEventElapsedTime(&cuda_elapsed_time, cuda_start, cuda_stop);
-
+  cudaMemcpy(seq_result, cuda_result, sizeof(short), cudaMemcpyDeviceToHost);
+  
   // destroy timers
   cudaEventDestroy(cuda_start);
   cudaEventDestroy(cuda_stop);
@@ -96,8 +99,8 @@ int main()
   cout << "Starting sequential iterative approach." << endl;
 
   // reset visited_matrix back to false
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (unsigned long long i = 0; i < N; i++) {
+    for (unsigned long long j = 0; j < N; j++) {
       visited_matrix[i][j] = false;
     }
   }
@@ -113,14 +116,14 @@ int main()
 
   cout << "----------------------------------------------------------" << endl;
   cout << "Found: " << *seq_result << endl;
-  cout << "[SEQUENTIAL - Iterative] Elapsed time: " << seq_elapsed_time << " clock cycles" << endl;
+  cout << "[SEQUENTIAL - Iterative] Elapsed time: " << seq_iter_elapsed_time << " clock cycles" << endl;
   cout << "----------------------------------------------------------" << endl;
 
   cout << "Starting sequential recursive approach." << endl;
 
   // reset visited_matrix back to false
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (unsigned long long i = 0; i < N; i++) {
+    for (unsigned long long j = 0; j < N; j++) {
       visited_matrix[i][j] = false;
     }
   }
@@ -132,45 +135,48 @@ int main()
   *seq_result = recursive_bfs(seq_matrix, N, target, visited_matrix);
 
   seq_stop = clock();
-  seq_recur_time = 1000*(seq_stop - seq_start)/CLOCKS_PER_SEC;
+  seq_recur_elapsed_time = 1000*(seq_stop - seq_start)/CLOCKS_PER_SEC;
 
   cout << "----------------------------------------------------------" << endl;
   cout << "Found: " << *seq_result << endl;
-  cout << "[SEQUENTIAL - Recursive] Elapsed time: " << seq_elapsed_time << " clock cycles" << endl;
+  cout << "[SEQUENTIAL - Recursive] Elapsed time: " << seq_recur_elapsed_time << " clock cycles" << endl;
   cout << "----------------------------------------------------------" << endl;
 
   // free and cuda free
-  for (int i = 0; i < N; i++) {
+  for (unsigned long long i = 0; i < N; i++) {
     free(seq_matrix[i]);
     free(visited_matrix[i]);
   }
   free(seq_matrix);
   free(visited_matrix);
   free(seq_result);
-  for (int i = 0; i < N; i++) {
+  for (unsigned long long i = 0; i < N; i++) {
     cudaFree(cuda_matrix[i]);
     cudaFree(cuda_visited_matrix[i]);
   }
   cudaFree(cuda_matrix);
+  cudaFree(mtx);
   cudaFree(cuda_visited_matrix);
   cudaFree(cuda_result);
 
   return 0;
 }
 
-int iterative_bfs(int **matrix, unsigned int N, int target, bool **visited_matrix) {
+short iterative_bfs(short **matrix, unsigned long long N, short target, bool **visited_matrix) {
   // check initial spot
   if (matrix[0][0] == target)
     return matrix[0][0];
 
   visited_matrix[0][0] = true;
-  queue<int> qx = new queue<int>;
-  queue<int> qy = new queue<int>;
+  queue<unsigned long long> qx;
+  queue<unsigned long long> qy;
   qx.push(0);
   qy.push(0);
   while (!qx.empty()) {
-    int x = qx.pop();
-    int y = qy.pop();
+    unsigned long long x = qx.front();
+    unsigned long long y = qy.front();
+    qx.pop();
+    qy.pop();
     visited_matrix[x][y] = true;
     if (matrix[x][y] == target) {
       return matrix[x][y];
@@ -188,17 +194,17 @@ int iterative_bfs(int **matrix, unsigned int N, int target, bool **visited_matri
   return -1;
 }
 
-int recursive_bfs(int **matrix, unsigned int N, int target, bool **visited_matrix) {
+short recursive_bfs(short **matrix, unsigned long long N, short target, bool **visited_matrix) {
   // check initial spot
   if (matrix[0][0] == target)
     return matrix[0][0];
 
   visited_matrix[0][0] = true;
 
-  return recursive_bfs_helper(matrix, N, target, 0, 0);
+  return recursive_bfs_helper(matrix, N, target, 0, 0, visited_matrix);
 }
 
-int recursive_bfs_helper(int **matrix, unsigned int N, int target, int x, int y, bool **visited_matrix) {
+short recursive_bfs_helper(short **matrix, unsigned long long N, short target, unsigned long long x, unsigned long long y, bool **visited_matrix) {
   // check right
   if (matrix[x+1][y] == target)
     return matrix[x+1][y];
@@ -207,12 +213,12 @@ int recursive_bfs_helper(int **matrix, unsigned int N, int target, int x, int y,
     return matrix[x][y+1];
 
   // traverse to right first, then down
-  int right = -1;
-  int down = -1;
+  short right = -1;
+  short down = -1;
 
   if (x+1 < N && !visited_matrix[x+1][y]) {
     visited_matrix[x+1][y] = true;
-    right = recursive_bfs_helper(matrix, N, target, x+1, y);
+    right = recursive_bfs_helper(matrix, N, target, x+1, y, visited_matrix);
   }
   // if found in right, then return right
   if (right != -1)
@@ -220,7 +226,7 @@ int recursive_bfs_helper(int **matrix, unsigned int N, int target, int x, int y,
 
   if (y+1 < N && !visited_matrix[x][y+1]) {
     visited_matrix[x][y+1] = true;
-    down = recursive_bfs_helper(matrix, N, target, x, y+1);
+    down = recursive_bfs_helper(matrix, N, target, x, y+1, visited_matrix);
   }
   // if found in down, then return down
   if (down != -1)
@@ -230,18 +236,18 @@ int recursive_bfs_helper(int **matrix, unsigned int N, int target, int x, int y,
   return -1;
 }
 
-void kernel_bfs_wrapper(int **matrix, int *result, int *mtx, unsigned int N, int target, bool **visited_matrix)
+void kernel_bfs_wrapper(short **matrix, short *result, int *mtx, unsigned long long N, short target)
 {
   // 2 dimensional
-  dim3 gridSize = (256, 256);
-  dim3 blockSize = (256, 256);
-  find_target_kernel<<< gridSize, blockSize >>>(matrix, max, mtx, N);
+  dim3 blockSize = (128, 128);
+  dim3 gridSize = (N/128, N/128);
+  bfs_kernel<<< gridSize, blockSize >>>(matrix, result, mtx, N, target);
 }
 
-__global__ void find_target_kernel(int **matrix, int *result, int *mtx, unsigned int N, int target, bool **visited_matrix)
+__global__ void bfs_kernel(short **matrix, short *result, int *mtx, unsigned long long N, short target)
 {
-  int idx = threadIdx.x + blockDim.x + blockIdx.x;
-  int idy = threadIdx.y + blockDim.y + blockIdx.y;
+  unsigned long long idx = threadIdx.x + blockDim.x + blockIdx.x;
+  unsigned long long idy = threadIdx.y + blockDim.y + blockIdx.y;
 
   if (idx >= N || idy >= N) {
     return;
